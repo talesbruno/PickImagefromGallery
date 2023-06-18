09350baf60a9382f4b204a196a4a3bd4d0698895
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2023-06-18 19:43:51 -0300

    erros ao implemntar em jetpack compose nova tentativa

--- app/src/main/java/co/talesbruno/pickimagefromgallery/data/repository/SelectImageImpl.kt
+++ app/src/main/java/co/talesbruno/pickimagefromgallery/data/repository/SelectImageImpl.kt
@@ -2,38 +2,23 @@ package co.talesbruno.pickimagefromgallery.data.repository
 
 
 import android.net.Uri
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
 import co.talesbruno.pickimagefromgallery.domain.repository.SelectImage
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
 class SelectImageImpl @Inject constructor(
-    private val registry : ActivityResultRegistry
-) : DefaultLifecycleObserver, SelectImage {
-    private lateinit var getContent : ActivityResultLauncher<String>
-    private lateinit var imageUriFlow: Flow<Uri?>
-    override fun onCreate(owner: LifecycleOwner) {
-        getContent = registry.register("key", owner, ActivityResultContracts.GetContent()) { uri ->
-            owner.lifecycleScope.launch {
-                imageUriFlow = flowOf(uri)
-            }
+    activity: ComponentActivity
+) : SelectImage {
+    private lateinit var imageUriFlow: Uri
+    private val getContent = activity.registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+            imageUriFlow = uri
         }
     }
 
-    override suspend fun selectImage(): Uri? {
+    override suspend fun selectImage(): Uri {
         getContent.launch("image/*")
-        return imageUriFlow.first()
+        return imageUriFlow
     }
 }
--- app/src/main/java/co/talesbruno/pickimagefromgallery/di/SelectImageModule.kt
+++ app/src/main/java/co/talesbruno/pickimagefromgallery/di/SelectImageModule.kt
@@ -1,19 +1,21 @@
 package co.talesbruno.pickimagefromgallery.di
 
-import androidx.activity.result.ActivityResultRegistry
+
+
+import androidx.activity.ComponentActivity
 import co.talesbruno.pickimagefromgallery.data.repository.SelectImageImpl
 import co.talesbruno.pickimagefromgallery.domain.repository.SelectImage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 
 
 @Module
-@InstallIn(ActivityComponent::class)
-object SelectImageModule {
+@InstallIn(SingletonComponent::class)
+object AppModule {
     @Provides
-    fun provideSelectImage(registry: ActivityResultRegistry): SelectImage {
-        return SelectImageImpl(registry)
+    fun provideSelectImage(activity: ComponentActivity): SelectImage {
+        return SelectImageImpl(activity)
     }
 }
