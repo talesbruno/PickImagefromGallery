f117d6f2147ab8207b71c4abec2f0aab116ac31a
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2023-06-17 22:16:09 -0300

    add coil

--- app/src/main/java/co/talesbruno/pickimagefromgallery/presentation/screens/MainScreen.kt
+++ app/src/main/java/co/talesbruno/pickimagefromgallery/presentation/screens/MainScreen.kt
@@ -1,18 +1,23 @@
 package co.talesbruno.pickimagefromgallery.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import co.talesbruno.pickimagefromgallery.presentation.viewmodel.SelectImageViewModel
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(selectImageViewModel: SelectImageViewModel){
+    val imageUri by selectImageViewModel.selectedImageUri.collectAsStateWithLifecycle()
     Scaffold() {
         Column() {
+            AsyncImage(model = imageUri, contentDescription = null)
             Button(onClick = { selectImageViewModel.selectImage() }) {
                 Text(text = "Select Image")
             }
